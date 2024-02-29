a5353fbab2664e05bf2835c5404db5fed80cd082
Isaque Franklin Amaral <isaquefrankli@gmail.com>
2024-02-29 15:41:15 -0300

    api done?

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"net/http"
 	"github.com/gin-gonic/gin"
-	//"errors"
+	"errors"
 )
 
 type book struct {
@@ -29,9 +29,34 @@ func bookById(c *gin.Context){
 	book, err := getBookById(id)
 
 	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found."})
 		return
 	}
 
+	c.IndentedJSON(http.StatusOK, book)
+}
+
+func checkoutBook(c *gin.Context){
+	id, ok := c.GetQuery("id")
+
+	if !ok {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Missing id query parameter."})
+		return
+	}
+
+	book, err := getBookById(id)
+	
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found."})
+		return
+	}
+
+	if book.Quantity <= 0 {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Book not avaliable."})
+		return
+	}
+
+	book.Quantity -= 1
 	c.IndentedJSON(http.StatusOK, book)
 }
 
@@ -61,5 +86,6 @@ func main(){
 	router.GET("/books", getBooks)
 	router.POST("/books", createBook)
 	router.GET("/books/:id", bookById)
+	router.PATCH("/checkout", checkoutBook)
 	router.Run("localhost:8080")
 }
